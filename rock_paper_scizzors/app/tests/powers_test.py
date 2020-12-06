b9e772f3f906d91a61f388a853ff52e4547e87c4
Evelyn Szarek <evelynszarek@gmail.com>
2020-12-06 18:39:41 +0000

    test scizzors NOT beat rock PASSED

--- rock_paper_scizzors/app/tests/powers_test.py
+++ rock_paper_scizzors/app/tests/powers_test.py
@@ -12,8 +12,12 @@ class TestPowers(unittest.TestCase):
     def test_rock_beats_scizzors(self):
         self.assertEqual(self.rock, winning_power(self.rock, self.scizzors))
 
+    def test_scizzors_not_beat_rock(self):
+        self.assertEqual(self.rock, winning_power(self.scizzors, self.rock))
+
     def test_scizzors_beat_paper(self):
         self.assertEqual(self.scizzors, winning_power(self.scizzors, self.paper))
 
     def test_paper_beats_rock(self):
         self.assertEqual(self.paper, winning_power(self.paper, self.rock))
+
