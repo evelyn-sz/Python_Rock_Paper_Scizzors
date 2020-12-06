d06201df03c5cfe37d4758d5324ca9c3189fbbc5
Evelyn Szarek <evelynszarek@gmail.com>
2020-12-06 22:39:32 +0000

    began app routes in controller

--- rock_paper_scizzors/app/controller.py
+++ rock_paper_scizzors/app/controller.py
@@ -1,10 +1,16 @@
 from flask import render_template, request, redirect
 from app import app
-from app.models.game import *
-from app.models.player import *
+from app.models.game import Game
+from app.models.player import Player
 
 @app.route('/')
 def index():
     return render_template('index.html', title='Home')
 
 # @app.route('/rock', methods=['POST'])
+# def play_game():
+#     player_1 = request.form['name']
+#     player_2 = request.form['name']
+#     game_played = Game(player_1=player_1, player_2=player_2)
+#     winning_power(player_1, player_2)
+#     return redirect('/')
--- rock_paper_scizzors/app/models/game.py
+++ rock_paper_scizzors/app/models/game.py
@@ -16,16 +16,3 @@ class Game:
 
         return player_2.name
 
-
-
-# def winning_power(power_1, power_2):
-#     if power_1.name == "Rock" and power_2.name == "Scizzors":
-#         return power_1
-
-#     if power_1.name == "Scizzors" and power_2.name == "Paper":
-#         return power_1
-
-#     if power_1.name == "Paper" and power_2.name == "Rock":
-#         return power_1
-
-#     return power_2
